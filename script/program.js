e75149965a20ee928e50a6e31c546a4c452244a4
Eric Hung <hsuyen.hung@gmail.com>
2014-10-25 13:44:40 +0800

    1st commit on 2014-10-25

--- script/program.js
+++ script/program.js
@@ -14,16 +14,19 @@ console.log(sum);
 /*
 // ex-03
 var fs = require("fs");
-var buf = fs.readFileSync("ex-03.txt");
+var buf = fs.readFileSync(process.argv[2]);
 var string = buf.toString();
 var strAry = string.split("\n");
-console.log(strAry.length);
+console.log(strAry.length-1);
 */
 
 /*
 // ex-04
 var fs = require("fs");
-var buf = fs.readFile(process.argv[2], function a(err, data){
+var buf = fs.readFile(process.argv[2], {encoding:"utf8"}, 
+function (err, data){
+    if (err) throw err;
+    
     var string = data.toString();
     var strAry = string.split("\n");
     console.log(strAry.length-1);
@@ -70,4 +73,65 @@ http.get(process.argv[2], function(res) {
 }).on('error', function(e) {
   console.log("Got error: " + e.message);
 }); 
+*/
+
+/*
+// ex-08
+var http = require("http");
+var str = "";
+http.get(process.argv[2], function(res) {
+  res.on('data', function(chunk) {
+    str += chunk.toString();
+  });
+  res.on('end', function() {
+    console.log(str.length);
+    console.log(str);
+  });
+}).on('error', function(e) {
+  console.log("Got error: " + e.message);
+}); 
+*/
+
+/*
+// ex-09
+var http = require("http");
+var contents = [];
+var status = [];
+
+
+function abc(url, i) {
+  http.get(url, function(res) {
+    res.on('data', function(chunk) {
+      contents[i] += chunk.toString();
+    });
+    
+    res.on('end', function() {
+      status[i] = "o";
+      if(checkPass() == "o"){
+        for(x=0; x<contents.length; x++){
+          console.log(contents[x]);
+        }
+      }
+    });
+    
+    res.on('error', function(e) {
+      console.log("Got error: " + e.message);
+    });   
+  });
+}
+
+function checkPass(){
+  var s = "o";
+  for(i=0; i<(process.argv.length-2); i++){
+    if(status[i] == "x")
+      s = "x";
+  }
+  return s;  
+}  
+
+for(i=0; i<(process.argv.length-2); i++){
+  contents[i] = "";
+  status[i] = "x";
+  abc(process.argv[i+2], i);
+}
 */
